ee8ea9d316ead2f85c962d9773a51ca18352cd0c
Noon Silk <noonsilk@gmail.com>
2014-06-07 16:05:08 +1000

    python script to generate tex file that latexmk can build.

---
+++ build_mainfile.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import sys
+
+
+def build_tex (filename):
+    tex = r"""\input{preamble}
+\input{header}
+\title{\input{title}}
+
+\begin{document}
+\maketitle
+"""
+
+    inputs = []
+    with open("layout.md", "r") as f:
+        lines = f.readlines()
+        for l in lines:
+            inputs.append("\input{" + l.strip('\r\n') + "}")
+
+    tex += ("\n".join(inputs))
+    tex += "\n\end{document}"
+
+    with open(filename, 'w') as f:
+        f.write(tex)
+
+if __name__ == "__main__":
+    full_dirname = os.path.dirname(os.path.realpath(__file__))
+    _, dirname = os.path.split(full_dirname)
+    build_tex(dirname + ".tex")
